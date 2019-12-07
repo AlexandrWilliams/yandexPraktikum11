55defa36496e3935288821946b6c8fac41357895
AlexandrWilliams <canablak@gmail.com>
2019-12-06 21:21:42 -0500

    replace markup to json

--- routes/cards.js
+++ routes/cards.js
@@ -3,31 +3,7 @@ const fs = require('fs');
 const cards = require('../data/cards.json');
 
 const routerCards = (req, res) => {
-      res.send(markupForCards(cards));
+      res.send(cards);
 };
-
-const cardMarkup = card =>  `
-            <div>
-              <p>${card.name}</p>
-              <p>${card.link}</p>
-              <p>${card.owner}</p>
-              <p>${card._id}</p>
-              <br>n     
-            </div>
-          `;
-
-const markupForCards = cards => `<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Cards</title>
-  </head>
-  <body>
-      <div>
-        ${cards.map(cardMarkup).join('')}
-      </div>
-  </body>
-  </html>
-`;
 
 module.exports = routerCards;
--- routes/users.js
+++ routes/users.js
@@ -3,39 +3,13 @@ const fs = require('fs');
 const users = require('../data/users.json');
 
 const routerUsers = (req, res) => {
-      res.send(markupForUsers(users));
+      res.send(users);
 };
-
-const userMarkup = user =>  `
-            <div>
-              <p>${user.name}</p>
-              <p>${user.about}</p>
-              <p>${user.avatar}</p>
-              <p>${user._id}</p>
-              <br>n     
-            </div>
-          `;
-
-const markupForUsers = users => `<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Users</title>
-  </head>
-  <body>
-      <div>
-        ${users.map(userMarkup).join('')}
-      </div>
-  </body>
-  </html>
-`;
 
 const userId = (req, res) => {
   const {id} = req.params;
 
-  let user;
-
-  users.forEach((e) => {if(e._id === id){user = e}});
+  let user = users.find(item => item._id == id);
 
   if (user == undefined) {
     res.send({error: `No users ${id}`});
