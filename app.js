b1e9192bc79119cf7905c553379b47a2f2d46f42
AlexandrWilliams <canablak@gmail.com>
2020-01-30 23:01:04 -0500

    google pain

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { errors } = require('celebrate');
 //winston blue
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 80 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
